56db39d69e42ec5ac5580d8bfe51b789921001aa
root <root@iZ948ell951Z.(none)>
2015-11-26 21:54:51 +0800

    test

---
+++ build/Source/CMakeFiles/CommUtil.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CommUtil.dir/Log.cpp.o"
+  "../../Lib/libCommUtil.pdb"
+  "../../Lib/libCommUtil.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CommUtil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
