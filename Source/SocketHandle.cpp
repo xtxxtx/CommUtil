b8a7396dc1038f8a0bf5ccba4e72ee48c8402578
xtxxtx <txxiao@live.com>
2017-12-14 15:07:23 +0800

    Update SocketHandle.cpp

--- Source/SocketHandle.cpp
+++ Source/SocketHandle.cpp
@@ -22,3 +22,39 @@ IHandle::~IHandle()
 	}
 }
 
+void
+IHandle::Set(int iFd)
+{
+	if (iFd < 1) {
+		return;
+	}
+	
+	if (m_iFd > 0) {
+		close(m_iFd);	
+	}
+	
+	m_iFd = iFd;
+}
+
+int
+IHandle::Add(int iEpoll)
+{
+	if (iEpoll < 1) {
+		return -1;	
+	}
+	
+	m_ee.events = EPOLLIN;
+	if (epoll_ctl(iEpoll, EPOLL_CTL_ADD, m_iFd, &m_ee) == -1) {
+		if (errno == EEXIST) {
+			epoll_ctl(iEpoll, EPOLL_CTL_MOD, m_iFd, &m_ee);	
+		} else {
+			OnClose();
+			return -1;
+		}
+	}
+	
+	m_iEpoll = iEpoll;
+	
+	return 0;
+}
+
