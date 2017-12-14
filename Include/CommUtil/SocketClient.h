2cd9b1cfd8664530caffc8b44dfb5f6511a9a016
xtxxtx <txxiao@live.com>
2017-12-14 16:44:31 +0800

    Update SocketClient.h

--- Include/CommUtil/SocketClient.h
+++ Include/CommUtil/SocketClient.h
@@ -4,8 +4,20 @@
 #include <deque>
 
 #include "Mutex.h"
+#include "SocketHandle.h"
+#include "PacketManager.h"
 
 typedef	void (*CBPush)(void*, PACKET*);
+
+////////////////////////////////////////////////////////
+class CSocketClient : public IHandle
+{
+	enum { BUF_SIZ=8192 };
+	
+public:
+	CSocketClient();
+	~CSocketClient();
+};
 
 #endif	// SOCKETCLIENT_H_
 
