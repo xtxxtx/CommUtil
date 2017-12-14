4b58219a402e9c80d22bf127deba38b15f200fb2
xtxxtx <txxiao@live.com>
2017-12-14 14:00:50 +0800

    Update SocketHandle.h

--- Include/CommUtil/SocketHandle.h
+++ Include/CommUtil/SocketHandle.h
@@ -2,8 +2,32 @@
 #ifndef SOCKETHANDLE_H_
 #define SOCKETHANDLE_H_
 
+#include <sys/epoll.h>
 
-
-
+class IHandle
+{
+public:
+  IHandle();
+  virtual ~IHandle();
+  
+  void    Set(int iFd);
+  
+  int     Add(int iEpoll);
+  void    Del();
+  
+  int&    GetFD() { return m_iFd; }
+  struct epoll_event* GetEE() { return m_ee; }
+  
+  void    Close();
+  
+public:
+  virtual int   OnRecv()  { return -1; }
+  virtual int   OnSend()  { return -1; }
+  
+protected:
+  struct epoll_event  m_ee;
+  int       m_iFd;
+  int       m_iEpoll;
+};
 
 #endif  // SOCKETHANDLE_H_
