00f66482c00b926cd4ac9b5714fe890c0b962ec9
xtxxtx <txxiao@163.com>
2017-12-14 14:49:47 +0800

    update SocketHandle.cpp

--- Source/SocketHandle.cpp
+++ Source/SocketHandle.cpp
@@ -1,5 +1,24 @@
 //
-//
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
 #include "CommUtil/SocketHandle.h"
 
+IHandle::IHandle()
+{
+	m_iFd		= -1;
+	m_iEpoll	= -1;
+	
+	m_ee.events	= EPOLLIN;
+	m_ee.data.ptr	= this;
+}
+
+IHandle::~IHandle()
+{
+	if (m_iFd != -1) {
+		close(m_iFd);
+	}
+}
 
