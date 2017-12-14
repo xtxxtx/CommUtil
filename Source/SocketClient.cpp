355f6953a775133ce24727d6aa4b7894a2b4ef54
xtxxtx <txxiao@live.com>
2017-12-14 16:48:03 +0800

    Create SocketClient.cpp

---
+++ Source/SocketClient.cpp
@@ -0,0 +1,23 @@
+// SocketClient.cpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include "CommUtil/Log.h"
+#include "CommUtil/SocketClient.h"
+
+
+CSocketClient::CSocketClient()
+{
+}
+
+CSocketClient::~CSocketClient()
+{
+}
+
