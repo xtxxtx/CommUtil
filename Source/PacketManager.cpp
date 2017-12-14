61279f9c085569b450c74b5bfecc20947051aa3f
xtxxtx <txxiao@live.com>
2017-12-14 15:49:08 +0800

    Create PacketManager.cpp

---
+++ Source/PacketManager.cpp
@@ -0,0 +1,25 @@
+// PacketManager.cpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "CommUtil/Log.h"
+#include "CommUtil/PacketManager.h"
+
+SINGLETON_IMPLEMENT(CPacketManager)
+
+CPacketManager::CPacketManager()
+{
+}
+
+CPacketManager::~CPacketManager()
+{
+}
+
+void
+CPacketManager::Close()
+{
+
+}
+
