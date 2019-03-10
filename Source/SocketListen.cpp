82a0e0738791f89f331c180ffd69943ef4143c93
xtxxtx <txxiao@live.com>
2019-03-10 22:23:23 +0800

    modified:   Source/SocketListen.cpp

--- Source/SocketListen.cpp
+++ Source/SocketListen.cpp
@@ -60,7 +60,7 @@ CSocketListen::Initialize(const char* pszAddr, uint16_t iPort)
 			break;
 		}
 
-		return Run(1);
+		return Run(2);
 	} while (false);
 
 	close(m_iFd);
@@ -72,12 +72,12 @@ CSocketListen::Initialize(const char* pszAddr, uint16_t iPort)
 void
 CSocketListen::Close()
 {
-	WaitExit();
-
 	if (m_iFd > 0) {
 		close(m_iFd);
 		m_iFd = -1;
 	}
+
+	WaitExit();
 }
 
 void
@@ -93,13 +93,13 @@ CSocketListen::Execute()
 	char szAddr[256] = {0};
 	sockaddr_in sa;
 	socklen_t len = SAIN_SIZE;
-#if 0
-	struct pollfd pfds = {m_iFd, POLLIN, 0};
+
+//	struct pollfd pfds = {m_iFd, POLLIN, 0};
 
 	for (; m_bRun; ) {
-		if (poll(&pfds, 1, 100) < 1) {
-			continue;
-		}
+//		if (poll(&pfds, 1, 100) < 1) {
+//			continue;
+//		}
 
 		len = SAIN_SIZE;
 		iFd = accept(m_iFd, (sockaddr *)&sa, &len);
@@ -114,23 +114,6 @@ CSocketListen::Execute()
 		inet_ntop(AF_INET, &(sa.sin_addr), szAddr, sizeof(szAddr));
 		m_pServer->Create(iFd, szAddr, ntohs(sa.sin_port));
 	}
-#else
-	for (; m_bRun;) {
-		len = SAIN_SIZE;
-		iFd = accept(m_iFd, (sockaddr *)&sa, &len);
-		if (iFd == -1 && errno != EAGAIN) {
-			break;
-		}
-
-		setsockopt(iFd, SOL_SOCKET, SO_RCVBUF, (const char*)&BUF_SIZ, sizeof(BUF_SIZ));
-		setsockopt(iFd, SOL_SOCKET, SO_SNDBUF, (const char*)&BUF_SIZ, sizeof(BUF_SIZ));
-
-		szAddr[0] = 0;
-		inet_ntop(AF_INET, &(sa.sin_addr), szAddr, sizeof(szAddr));
-		m_pServer->Create(iFd, szAddr, ntohs(sa.sin_port));
-	}
-#endif
 
 	CLog::Instance()->Write(LOG_INFO, "CSocketListen::Execute() exit.");
 }
-
